456bbb0ecf93606943deab499ac58c6dbe71a4a0
Adnan Ahmad <viperadnan@gmail.com>
2025-12-11 22:27:57 +0000

    chore: remove obsolete installer scripts and configuration files

--- cmd/gpcli/generate-installer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-$(go env GOPATH)/bin/binst init --source=goreleaser --file=.goreleaser.yml -o binstaller.yml --name $(basename $(pwd)) --repo viperadnan-git/gogpm
-$(go env GOPATH)/bin/binst gen --config=binstaller.yml -o install.sh
